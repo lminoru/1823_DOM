22274315577218b867a22e7779052f96a40919ad
Leonardo Minoru Iwashima <CIANDT\liwashima@lnb028452cps.ciandt.global>
2021-03-25 14:55:05 -0300

    concluir tarefa

--- 1823_Aula1/listaDeTarefas.js
+++ 1823_Aula1/listaDeTarefas.js
@@ -1,28 +1,50 @@
-const criarTarefa = (evento) => {
+(() => {
+    const criarTarefa = (evento) => {
 
-    evento.preventDefault();
-   
-    const lista = document.querySelector('[data-list]');
-    const input = document.querySelector('[data-form-input]');
-    const valor = input.value;    
-
+        evento.preventDefault();
+       
+        const lista = document.querySelector('[data-list]');
+        const input = document.querySelector('[data-form-input]');
+        const valor = input.value;    
     
-    const tarefa = document.createElement('li');
-    tarefa.classList.add('task');
-
-    const conteudo = `<p class="content">${valor}<p/>`;
-    // const box = `<li class="task">${conteudo}<li/>`;
-
-    tarefa.innerHTML = conteudo;
-
-    lista.appendChild(tarefa);
-    input.value = '';
-}
-
-
-const novaTarefa = document.querySelector('[data-form-button]');
-
-
-novaTarefa.addEventListener('click', criarTarefa)
-
-
+        
+        const tarefa = document.createElement('li');
+        tarefa.classList.add('task');
+    
+        const conteudo = `<p class="content">${valor}<p/>`;
+        // const box = `<li class="task">${conteudo}<li/>`;
+    
+        tarefa.innerHTML = conteudo;
+    
+        tarefa.appendChild(BotaoConclui());
+        lista.appendChild(tarefa);
+        input.value = '';
+    }
+    
+    
+    const novaTarefa = document.querySelector('[data-form-button]');
+    
+    
+    novaTarefa.addEventListener('click', criarTarefa);
+    
+    
+    const BotaoConclui = () => {
+        const botaoConclui = document.createElement('button');
+    
+        botaoConclui.classList.add('check-button');
+        botaoConclui.innerText = 'Done';
+    
+        botaoConclui.addEventListener('click', concluirTarefa);
+    
+        return botaoConclui;
+    };
+    
+    const concluirTarefa = (evento) => {
+        const botaoConclui = evento.target;
+    
+        const tarefaCompleta = botaoConclui.parentElement; //pai do botao
+    
+        tarefaCompleta.classList.toggle('done');
+    };
+    
+})()
