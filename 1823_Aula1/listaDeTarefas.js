5b22f49c1e1cdb1c8a6dabd739b952dabf8fdabb
Leonardo Minoru Iwashima <CIANDT\liwashima@lnb028452cps.ciandt.global>
2021-03-25 14:19:13 -0300

    adicionar itens a lista

--- 1823_Aula1/listaDeTarefas.js
+++ 1823_Aula1/listaDeTarefas.js
@@ -1,8 +1,21 @@
 const criarTarefa = (evento) => {
+
     evento.preventDefault();
+   
+    const lista = document.querySelector('[data-list]');
     const input = document.querySelector('[data-form-input]');
-    const valor = input.value;
-    document.querySelector('p').textContent = valor;
+    const valor = input.value;    
+
+    
+    const tarefa = document.createElement('li');
+    tarefa.classList.add('task');
+
+    const conteudo = `<p class="content">${valor}<p/>`;
+    // const box = `<li class="task">${conteudo}<li/>`;
+
+    tarefa.innerHTML = conteudo;
+
+    lista.appendChild(tarefa);
     input.value = '';
 }
 
